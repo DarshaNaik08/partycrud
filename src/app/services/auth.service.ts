611539bbe073a47b0ecbc38b8b5543272e68acb6
DarshaNaik08 <darshanaik08@gmail.com>
2024-06-06 19:16:52 +0530

    config changes

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 // import { httpheaders } from './httpheaders';
 import { tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // baseUrl: 'https://ap.greatfuturetechno.com',
-baseUrl: 'https://apgreatfuturetechno.netlify.app',
+  baseUrl: 'https://ap.greatfuturetechno.com',
 };
